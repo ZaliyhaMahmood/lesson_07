7ed45636a1f5ad859a5f483b8f73ac07ac160580
ZaliyhaMahmood <mzaliyha@yahoo.com>
2018-07-02 03:44:49 +0300

    First commit

---
+++ test/components/post-list.spec.js
@@ -0,0 +1,21 @@
+import { expect } from 'chai';
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import PostList from '../../components/post-list.react';
+import Post from '../../components/post.react';
+import sd from 'skin-deep';
+
+describe('PostList component', () => {
+  const postData = [
+    { id: 1, title: 'Title 1', content: '<p>Content 1</p>' },
+    { id: 2, title: 'Title 2', content: '<p>Content 2</p>' },
+    { id: 3, title: 'Title 3', content: '<p>Content 3</p>' }
+  ];
+
+  it('should render a list of post components', () => {
+    const tree = sd.shallowRender(React.createElement(PostList, {posts: postData}));
+    const items = tree.everySubTree('Post');
+
+    expect(items.length).to.equal(postData.length);
+  });
+});
---
+++ test/components/post.spec.js
@@ -0,0 +1,37 @@
+import {expect} from 'chai';
+import React from 'react';
+import Post from '../../components/post.react';
+import sd from 'skin-deep';
+
+describe('React Test', () => {
+  let tree;
+
+  beforeEach(() => {
+    tree = sd.shallowRender(React.createElement(Post, {title: 'Title', content: '<p>Content</p>'}));
+  });
+
+  it('should render a post title and content', () => {
+    expect(tree.subTree('.Post-header').text()).to.equal('Title');
+    expect(tree.subTree('.Post-content').text()).to.equal('Content');
+  });
+
+  describe('doSomethingOnClick method', () => {
+    it('should modify the `isClicked` state property', () => {
+      const header = tree.subTree('.Post-header');
+
+      header.props.onClick({
+        preventDefault() {}
+      });
+
+      expect(tree.getMountedInstance().state).to.eql({isClicked: true});
+    });
+  });
+
+  describe('stripParagraphTags method', () => {
+    it('should strip <p> tags', () => {
+      const strippedText = Post.prototype.stripParagraphTags('<p>Some text.</p> <p>More text.</p>');
+
+      expect(strippedText).to.equal('Some text. More text.');
+    });
+  });
+});
